fc0747fbf9a8b43c5c3db3a36ad4a6a287f11b7a
Andrea Sorbini <asorbini@rti.com>
2020-12-22 19:28:12 -0800

    uvnd_cell

--- bin/uvnd.profile.sh
+++ bin/uvnd.profile.sh
@@ -45,7 +45,12 @@ uvnd_stop()
 uvnd_restart()
 {
     uvnd_stop
-    uvnd_restart "$@"
+    uvnd_start "$@"
+}
+
+uvnd_cell()
+{
+    uvnd_start "-n $@"
 }
 
 uvnd_pid()
@@ -118,7 +123,8 @@ uvnd_help()
     (
     echo "  uvnd_start:     start uvn agent in a screen session (resume session if already started)"
     echo "  uvnd_stop:      terminate uvn agent's session, and wait for it to exit."
-    echo "  uvnd_restart:   stop and restart uvn agent's session"
+    echo "  uvnd_restart:   restart uvn agent's session"
+    echo "  uvnd_cell:      same as uvnd_start, but also enables the nameserver"
     echo "  uvnd_pid:       print PID of active uvn agent"
     echo "  uvnd_kill:      send a signal to uvn agent"
     echo "  uvnd_deploy:    signal uvn agent to generate a new deployment"
